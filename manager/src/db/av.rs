8614644b72504979ad4ff6a2b68dc2b9b0cdac09
LolaInTa Chi <lolainta@gmail.com>
2026-01-27 14:26:44 +0800

    fix: remove db layer dependency (dto)

--- manager/src/db/av.rs
+++ manager/src/db/av.rs
@@ -1,13 +1,13 @@
-use crate::{entity::av, http::dto::av::CreateAvRequest};
+use crate::entity::av;
 use sea_orm::*;
 
 pub async fn find_all(db: &DatabaseConnection) -> Result<Vec<av::Model>, DbErr> {
     av::Entity::find().all(db).await
 }
 
-pub async fn create(db: &DatabaseConnection, req: CreateAvRequest) -> Result<av::Model, DbErr> {
+pub async fn create(db: &DatabaseConnection, name: String) -> Result<av::Model, DbErr> {
     let active = av::ActiveModel {
-        name: Set(req.name),
+        name: Set(name),
         ..Default::default()
     };
 
